8bdf7349b3967001a035986716b2f21ec338507c
Prateek Chhikara <prateekchhikara2012@gmail.com>
2023-07-07 22:20:02 +0530

    tep

---
+++ temp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class temp
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("temp file only");
+        sc.close();
+    }
+}
