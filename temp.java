a28f8cba851cd31f10661e3c0871c19ef2c7dabd
Prateek Chhikara <prateekchhikara2012@gmail.com>
2023-07-07 22:21:51 +0530

    5 comit

--- temp.java
+++ temp.java
@@ -5,6 +5,11 @@ public class temp
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("temp file only");
+        System.out.println("Hello");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("hiii");
+        }
         sc.close();
     }
 }
